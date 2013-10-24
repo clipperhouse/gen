f05b9246b94693901e2720b6c9c313b8ce1dbf6c
Matt Sherman <mwsherman@gmail.com>
2013-10-23 22:50:52 -0400

    use type-specific names for sort support funcs

--- templates.go
+++ templates.go
@@ -94,57 +94,82 @@ func ({{.Receiver}} {{.Plural}}) Where(fn func({{.Pointer}}{{.Singular}}) bool) 
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-func swap({{.Receiver}} {{.Plural}}, a, b int) {
+func ({{.Receiver}} {{.Plural}}) SortByString(fn func({{.Pointer}}{{.Singular}}) string) {{.Plural}} {
+	result := make({{.Plural}}, len({{.Receiver}}))
+	copy(result, {{.Receiver}})
+
+	// Switch to heapsort if depth of 2*ceil(lg(n+1)) is reached.
+	n := len(result)
+	maxDepth := 0
+	for i := n; i > 0; i >>= 1 {
+		maxDepth++
+	}
+	maxDepth *= 2
+	quickSort{{.Plural}}(result, fn, 0, n, maxDepth)
+	return result
+}
+
+// IsSorted reports whether {{.Receiver}} is sorted.
+func ({{.Receiver}} {{.Plural}}) IsSorted(fn func({{.Pointer}}{{.Singular}}) string) bool {
+	n := len({{.Receiver}})
+	for i := n - 1; i > 0; i-- {
+		if less{{.Plural}}({{.Receiver}}, fn, i, i-1) {
+			return false
+		}
+	}
+	return true
+}
+func swap{{.Plural}}({{.Receiver}} {{.Plural}}, a, b int) {
 	{{.Receiver}}[a], {{.Receiver}}[b] = {{.Receiver}}[b], {{.Receiver}}[a]
 }
 
-func less({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b int) bool {
+func less{{.Plural}}({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b int) bool {
 	return fn({{.Receiver}}[a]) < fn({{.Receiver}}[b])
 }
 
 // Insertion sort
-func insertionSort({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b int) {
+func insertionSort{{.Plural}}({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b int) {
 	for i := a + 1; i < b; i++ {
-		for j := i; j > a && less({{.Receiver}}, fn, j, j-1); j-- {
-			swap({{.Receiver}}, j, j-1)
+		for j := i; j > a && less{{.Plural}}({{.Receiver}}, fn, j, j-1); j-- {
+			swap{{.Plural}}({{.Receiver}}, j, j-1)
 		}
 	}
 }
 
 // siftDown implements the heap property on {{.Receiver}}[lo, hi).
 // first is an offset into the array where the root of the heap lies.
-func siftDown({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, lo, hi, first int) {
+func siftDown{{.Plural}}({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, lo, hi, first int) {
 	root := lo
 	for {
 		child := 2*root + 1
 		if child >= hi {
 			break
 		}
-		if child+1 < hi && less({{.Receiver}}, fn, first+child, first+child+1) {
+		if child+1 < hi && less{{.Plural}}({{.Receiver}}, fn, first+child, first+child+1) {
 			child++
 		}
-		if !less({{.Receiver}}, fn, first+root, first+child) {
+		if !less{{.Plural}}({{.Receiver}}, fn, first+root, first+child) {
 			return
 		}
-		swap({{.Receiver}}, first+root, first+child)
+		swap{{.Plural}}({{.Receiver}}, first+root, first+child)
 		root = child
 	}
 }
 
-func heapSort({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b int) {
+func heapSort{{.Plural}}({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b int) {
 	first := a
 	lo := 0
 	hi := b - a
 
 	// Build heap with greatest element at top.
 	for i := (hi - 1) / 2; i >= 0; i-- {
-		siftDown({{.Receiver}}, fn, i, hi, first)
+		siftDown{{.Plural}}({{.Receiver}}, fn, i, hi, first)
 	}
 
 	// Pop elements, largest first, into end of {{.Receiver}}.
 	for i := hi - 1; i >= 0; i-- {
-		swap({{.Receiver}}, first, first+i)
-		siftDown({{.Receiver}}, fn, lo, i, first)
+		swap{{.Plural}}({{.Receiver}}, first, first+i)
+		siftDown{{.Plural}}({{.Receiver}}, fn, lo, i, first)
 	}
 }
 
@@ -152,39 +177,39 @@ func heapSort({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) stri
 // Engineering a Sort Function, SP&E November 1993.
 
 // medianOfThree moves the median of the three values {{.Receiver}}[a], {{.Receiver}}[b], {{.Receiver}}[c] into {{.Receiver}}[a].
-func medianOfThree({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b, c int) {
+func medianOfThree{{.Plural}}({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b, c int) {
 	m0 := b
 	m1 := a
 	m2 := c
 	// bubble sort on 3 elements
-	if less({{.Receiver}}, fn, m1, m0) {
-		swap({{.Receiver}}, m1, m0)
+	if less{{.Plural}}({{.Receiver}}, fn, m1, m0) {
+		swap{{.Plural}}({{.Receiver}}, m1, m0)
 	}
-	if less({{.Receiver}}, fn, m2, m1) {
-		swap({{.Receiver}}, m2, m1)
+	if less{{.Plural}}({{.Receiver}}, fn, m2, m1) {
+		swap{{.Plural}}({{.Receiver}}, m2, m1)
 	}
-	if less({{.Receiver}}, fn, m1, m0) {
-		swap({{.Receiver}}, m1, m0)
+	if less{{.Plural}}({{.Receiver}}, fn, m1, m0) {
+		swap{{.Plural}}({{.Receiver}}, m1, m0)
 	}
 	// now {{.Receiver}}[m0] <= {{.Receiver}}[m1] <= {{.Receiver}}[m2]
 }
 
-func swapRange({{.Receiver}} {{.Plural}}, a, b, n int) {
+func swapRange{{.Plural}}({{.Receiver}} {{.Plural}}, a, b, n int) {
 	for i := 0; i < n; i++ {
-		swap({{.Receiver}}, a+i, b+i)
+		swap{{.Plural}}({{.Receiver}}, a+i, b+i)
 	}
 }
 
-func doPivot({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, lo, hi int) (midlo, midhi int) {
+func doPivot{{.Plural}}({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, lo, hi int) (midlo, midhi int) {
 	m := lo + (hi-lo)/2 // Written like this to avoid integer overflow.
 	if hi-lo > 40 {
 		// Tukey's Ninther, median of three medians of three.
 		s := (hi - lo) / 8
-		medianOfThree({{.Receiver}}, fn, lo, lo+s, lo+2*s)
-		medianOfThree({{.Receiver}}, fn, m, m-s, m+s)
-		medianOfThree({{.Receiver}}, fn, hi-1, hi-1-s, hi-1-2*s)
+		medianOfThree{{.Plural}}({{.Receiver}}, fn, lo, lo+s, lo+2*s)
+		medianOfThree{{.Plural}}({{.Receiver}}, fn, m, m-s, m+s)
+		medianOfThree{{.Plural}}({{.Receiver}}, fn, hi-1, hi-1-s, hi-1-2*s)
 	}
-	medianOfThree({{.Receiver}}, fn, lo, m, hi-1)
+	medianOfThree{{.Plural}}({{.Receiver}}, fn, lo, m, hi-1)
 
 	// Invariants are:
 	//	{{.Receiver}}[lo] = pivot (set up by ChoosePivot)
@@ -200,10 +225,10 @@ func doPivot({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) strin
 	a, b, c, d := lo+1, lo+1, hi, hi
 	for {
 		for b < c {
-			if less({{.Receiver}}, fn, b, pivot) { // {{.Receiver}}[b] < pivot
+			if less{{.Plural}}({{.Receiver}}, fn, b, pivot) { // {{.Receiver}}[b] < pivot
 				b++
-			} else if !less({{.Receiver}}, fn, pivot, b) { // {{.Receiver}}[b] = pivot
-				swap({{.Receiver}}, a, b)
+			} else if !less{{.Plural}}({{.Receiver}}, fn, pivot, b) { // {{.Receiver}}[b] = pivot
+				swap{{.Plural}}({{.Receiver}}, a, b)
 				a++
 				b++
 			} else {
@@ -211,10 +236,10 @@ func doPivot({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) strin
 			}
 		}
 		for b < c {
-			if less({{.Receiver}}, fn, pivot, c-1) { // {{.Receiver}}[c-1] > pivot
+			if less{{.Plural}}({{.Receiver}}, fn, pivot, c-1) { // {{.Receiver}}[c-1] > pivot
 				c--
-			} else if !less({{.Receiver}}, fn, c-1, pivot) { // {{.Receiver}}[c-1] = pivot
-				swap({{.Receiver}}, c-1, d-1)
+			} else if !less{{.Plural}}({{.Receiver}}, fn, c-1, pivot) { // {{.Receiver}}[c-1] = pivot
+				swap{{.Plural}}({{.Receiver}}, c-1, d-1)
 				c--
 				d--
 			} else {
@@ -225,7 +250,7 @@ func doPivot({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) strin
 			break
 		}
 		// {{.Receiver}}[b] > pivot; {{.Receiver}}[c-1] < pivot
-		swap({{.Receiver}}, b, c-1)
+		swap{{.Plural}}({{.Receiver}}, b, c-1)
 		b++
 		c--
 	}
@@ -238,60 +263,34 @@ func doPivot({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) strin
 	}
 
 	n := min(b-a, a-lo)
-	swapRange({{.Receiver}}, lo, b-n, n)
+	swapRange{{.Plural}}({{.Receiver}}, lo, b-n, n)
 
 	n = min(hi-d, d-c)
-	swapRange({{.Receiver}}, c, hi-n, n)
+	swapRange{{.Plural}}({{.Receiver}}, c, hi-n, n)
 
 	return lo + b - a, hi - (d - c)
 }
 
-func quickSort({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b, maxDepth int) {
+func quickSort{{.Plural}}({{.Receiver}} {{.Plural}}, fn func({{.Pointer}}{{.Singular}}) string, a, b, maxDepth int) {
 	for b-a > 7 {
 		if maxDepth == 0 {
-			heapSort({{.Receiver}}, fn, a, b)
+			heapSort{{.Plural}}({{.Receiver}}, fn, a, b)
 			return
 		}
 		maxDepth--
-		mlo, mhi := doPivot({{.Receiver}}, fn, a, b)
+		mlo, mhi := doPivot{{.Plural}}({{.Receiver}}, fn, a, b)
 		// Avoiding recursion on the larger subproblem guarantees
 		// a stack depth of at most lg(b-a).
 		if mlo-a < b-mhi {
-			quickSort({{.Receiver}}, fn, a, mlo, maxDepth)
-			a = mhi // i.e., quickSort({{.Receiver}}, mhi, b)
+			quickSort{{.Plural}}({{.Receiver}}, fn, a, mlo, maxDepth)
+			a = mhi // i.e., quickSort{{.Plural}}({{.Receiver}}, mhi, b)
 		} else {
-			quickSort({{.Receiver}}, fn, mhi, b, maxDepth)
-			b = mlo // i.e., quickSort({{.Receiver}}, a, mlo)
+			quickSort{{.Plural}}({{.Receiver}}, fn, mhi, b, maxDepth)
+			b = mlo // i.e., quickSort{{.Plural}}({{.Receiver}}, a, mlo)
 		}
 	}
 	if b-a > 1 {
-		insertionSort({{.Receiver}}, fn, a, b)
+		insertionSort{{.Plural}}({{.Receiver}}, fn, a, b)
 	}
-}
-
-func ({{.Receiver}} {{.Plural}}) SortByString(fn func({{.Pointer}}{{.Singular}}) string) {{.Plural}} {
-	result := make({{.Plural}}, len({{.Receiver}}))
-	copy(result, {{.Receiver}})
-
-	// Switch to heapsort if depth of 2*ceil(lg(n+1)) is reached.
-	n := len(result)
-	maxDepth := 0
-	for i := n; i > 0; i >>= 1 {
-		maxDepth++
-	}
-	maxDepth *= 2
-	quickSort(result, fn, 0, n, maxDepth)
-	return result
-}
-
-// IsSorted reports whether {{.Receiver}} is sorted.
-func ({{.Receiver}} {{.Plural}}) IsSorted(fn func({{.Pointer}}{{.Singular}}) string) bool {
-	n := len({{.Receiver}})
-	for i := n - 1; i > 0; i-- {
-		if less({{.Receiver}}, fn, i, i-1) {
-			return false
-		}
-	}
-	return true
 }
 `
--- test/movie_gen.go
+++ test/movie_gen.go
@@ -1,6 +1,6 @@
 // gen *models.Movie
 // this file was auto-generated using github.com/clipperhouse/gen
-// Thu, 24 Oct 2013 02:23:53 UTC
+// Thu, 24 Oct 2013 02:50:20 UTC
 
 package models
 
@@ -84,57 +84,82 @@ func (rcv Movies) Where(fn func(*Movie) bool) (result Movies) {
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-func swap(rcv Movies, a, b int) {
+func (rcv Movies) SortByString(fn func(*Movie) string) Movies {
+	result := make(Movies, len(rcv))
+	copy(result, rcv)
+
+	// Switch to heapsort if depth of 2*ceil(lg(n+1)) is reached.
+	n := len(result)
+	maxDepth := 0
+	for i := n; i > 0; i >>= 1 {
+		maxDepth++
+	}
+	maxDepth *= 2
+	quickSortMovies(result, fn, 0, n, maxDepth)
+	return result
+}
+
+// IsSorted reports whether rcv is sorted.
+func (rcv Movies) IsSorted(fn func(*Movie) string) bool {
+	n := len(rcv)
+	for i := n - 1; i > 0; i-- {
+		if lessMovies(rcv, fn, i, i-1) {
+			return false
+		}
+	}
+	return true
+}
+func swapMovies(rcv Movies, a, b int) {
 	rcv[a], rcv[b] = rcv[b], rcv[a]
 }
 
-func less(rcv Movies, fn func(*Movie) string, a, b int) bool {
+func lessMovies(rcv Movies, fn func(*Movie) string, a, b int) bool {
 	return fn(rcv[a]) < fn(rcv[b])
 }
 
 // Insertion sort
-func insertionSort(rcv Movies, fn func(*Movie) string, a, b int) {
+func insertionSortMovies(rcv Movies, fn func(*Movie) string, a, b int) {
 	for i := a + 1; i < b; i++ {
-		for j := i; j > a && less(rcv, fn, j, j-1); j-- {
-			swap(rcv, j, j-1)
+		for j := i; j > a && lessMovies(rcv, fn, j, j-1); j-- {
+			swapMovies(rcv, j, j-1)
 		}
 	}
 }
 
 // siftDown implements the heap property on rcv[lo, hi).
 // first is an offset into the array where the root of the heap lies.
-func siftDown(rcv Movies, fn func(*Movie) string, lo, hi, first int) {
+func siftDownMovies(rcv Movies, fn func(*Movie) string, lo, hi, first int) {
 	root := lo
 	for {
 		child := 2*root + 1
 		if child >= hi {
 			break
 		}
-		if child+1 < hi && less(rcv, fn, first+child, first+child+1) {
+		if child+1 < hi && lessMovies(rcv, fn, first+child, first+child+1) {
 			child++
 		}
-		if !less(rcv, fn, first+root, first+child) {
+		if !lessMovies(rcv, fn, first+root, first+child) {
 			return
 		}
-		swap(rcv, first+root, first+child)
+		swapMovies(rcv, first+root, first+child)
 		root = child
 	}
 }
 
-func heapSort(rcv Movies, fn func(*Movie) string, a, b int) {
+func heapSortMovies(rcv Movies, fn func(*Movie) string, a, b int) {
 	first := a
 	lo := 0
 	hi := b - a
 
 	// Build heap with greatest element at top.
 	for i := (hi - 1) / 2; i >= 0; i-- {
-		siftDown(rcv, fn, i, hi, first)
+		siftDownMovies(rcv, fn, i, hi, first)
 	}
 
 	// Pop elements, largest first, into end of rcv.
 	for i := hi - 1; i >= 0; i-- {
-		swap(rcv, first, first+i)
-		siftDown(rcv, fn, lo, i, first)
+		swapMovies(rcv, first, first+i)
+		siftDownMovies(rcv, fn, lo, i, first)
 	}
 }
 
@@ -142,39 +167,39 @@ func heapSort(rcv Movies, fn func(*Movie) string, a, b int) {
 // Engineering a Sort Function, SP&E November 1993.
 
 // medianOfThree moves the median of the three values rcv[a], rcv[b], rcv[c] into rcv[a].
-func medianOfThree(rcv Movies, fn func(*Movie) string, a, b, c int) {
+func medianOfThreeMovies(rcv Movies, fn func(*Movie) string, a, b, c int) {
 	m0 := b
 	m1 := a
 	m2 := c
 	// bubble sort on 3 elements
-	if less(rcv, fn, m1, m0) {
-		swap(rcv, m1, m0)
+	if lessMovies(rcv, fn, m1, m0) {
+		swapMovies(rcv, m1, m0)
 	}
-	if less(rcv, fn, m2, m1) {
-		swap(rcv, m2, m1)
+	if lessMovies(rcv, fn, m2, m1) {
+		swapMovies(rcv, m2, m1)
 	}
-	if less(rcv, fn, m1, m0) {
-		swap(rcv, m1, m0)
+	if lessMovies(rcv, fn, m1, m0) {
+		swapMovies(rcv, m1, m0)
 	}
 	// now rcv[m0] <= rcv[m1] <= rcv[m2]
 }
 
-func swapRange(rcv Movies, a, b, n int) {
+func swapRangeMovies(rcv Movies, a, b, n int) {
 	for i := 0; i < n; i++ {
-		swap(rcv, a+i, b+i)
+		swapMovies(rcv, a+i, b+i)
 	}
 }
 
-func doPivot(rcv Movies, fn func(*Movie) string, lo, hi int) (midlo, midhi int) {
+func doPivotMovies(rcv Movies, fn func(*Movie) string, lo, hi int) (midlo, midhi int) {
 	m := lo + (hi-lo)/2 // Written like this to avoid integer overflow.
 	if hi-lo > 40 {
 		// Tukey's Ninther, median of three medians of three.
 		s := (hi - lo) / 8
-		medianOfThree(rcv, fn, lo, lo+s, lo+2*s)
-		medianOfThree(rcv, fn, m, m-s, m+s)
-		medianOfThree(rcv, fn, hi-1, hi-1-s, hi-1-2*s)
+		medianOfThreeMovies(rcv, fn, lo, lo+s, lo+2*s)
+		medianOfThreeMovies(rcv, fn, m, m-s, m+s)
+		medianOfThreeMovies(rcv, fn, hi-1, hi-1-s, hi-1-2*s)
 	}
-	medianOfThree(rcv, fn, lo, m, hi-1)
+	medianOfThreeMovies(rcv, fn, lo, m, hi-1)
 
 	// Invariants are:
 	//	rcv[lo] = pivot (set up by ChoosePivot)
@@ -190,10 +215,10 @@ func doPivot(rcv Movies, fn func(*Movie) string, lo, hi int) (midlo, midhi int) 
 	a, b, c, d := lo+1, lo+1, hi, hi
 	for {
 		for b < c {
-			if less(rcv, fn, b, pivot) { // rcv[b] < pivot
+			if lessMovies(rcv, fn, b, pivot) { // rcv[b] < pivot
 				b++
-			} else if !less(rcv, fn, pivot, b) { // rcv[b] = pivot
-				swap(rcv, a, b)
+			} else if !lessMovies(rcv, fn, pivot, b) { // rcv[b] = pivot
+				swapMovies(rcv, a, b)
 				a++
 				b++
 			} else {
@@ -201,10 +226,10 @@ func doPivot(rcv Movies, fn func(*Movie) string, lo, hi int) (midlo, midhi int) 
 			}
 		}
 		for b < c {
-			if less(rcv, fn, pivot, c-1) { // rcv[c-1] > pivot
+			if lessMovies(rcv, fn, pivot, c-1) { // rcv[c-1] > pivot
 				c--
-			} else if !less(rcv, fn, c-1, pivot) { // rcv[c-1] = pivot
-				swap(rcv, c-1, d-1)
+			} else if !lessMovies(rcv, fn, c-1, pivot) { // rcv[c-1] = pivot
+				swapMovies(rcv, c-1, d-1)
 				c--
 				d--
 			} else {
@@ -215,7 +240,7 @@ func doPivot(rcv Movies, fn func(*Movie) string, lo, hi int) (midlo, midhi int) 
 			break
 		}
 		// rcv[b] > pivot; rcv[c-1] < pivot
-		swap(rcv, b, c-1)
+		swapMovies(rcv, b, c-1)
 		b++
 		c--
 	}
@@ -228,59 +253,33 @@ func doPivot(rcv Movies, fn func(*Movie) string, lo, hi int) (midlo, midhi int) 
 	}
 
 	n := min(b-a, a-lo)
-	swapRange(rcv, lo, b-n, n)
+	swapRangeMovies(rcv, lo, b-n, n)
 
 	n = min(hi-d, d-c)
-	swapRange(rcv, c, hi-n, n)
+	swapRangeMovies(rcv, c, hi-n, n)
 
 	return lo + b - a, hi - (d - c)
 }
 
-func quickSort(rcv Movies, fn func(*Movie) string, a, b, maxDepth int) {
+func quickSortMovies(rcv Movies, fn func(*Movie) string, a, b, maxDepth int) {
 	for b-a > 7 {
 		if maxDepth == 0 {
-			heapSort(rcv, fn, a, b)
+			heapSortMovies(rcv, fn, a, b)
 			return
 		}
 		maxDepth--
-		mlo, mhi := doPivot(rcv, fn, a, b)
+		mlo, mhi := doPivotMovies(rcv, fn, a, b)
 		// Avoiding recursion on the larger subproblem guarantees
 		// a stack depth of at most lg(b-a).
 		if mlo-a < b-mhi {
-			quickSort(rcv, fn, a, mlo, maxDepth)
-			a = mhi // i.e., quickSort(rcv, mhi, b)
+			quickSortMovies(rcv, fn, a, mlo, maxDepth)
+			a = mhi // i.e., quickSortMovies(rcv, mhi, b)
 		} else {
-			quickSort(rcv, fn, mhi, b, maxDepth)
-			b = mlo // i.e., quickSort(rcv, a, mlo)
+			quickSortMovies(rcv, fn, mhi, b, maxDepth)
+			b = mlo // i.e., quickSortMovies(rcv, a, mlo)
 		}
 	}
 	if b-a > 1 {
-		insertionSort(rcv, fn, a, b)
+		insertionSortMovies(rcv, fn, a, b)
 	}
-}
-
-func (rcv Movies) SortByString(fn func(*Movie) string) Movies {
-	result := make(Movies, len(rcv))
-	copy(result, rcv)
-
-	// Switch to heapsort if depth of 2*ceil(lg(n+1)) is reached.
-	n := len(result)
-	maxDepth := 0
-	for i := n; i > 0; i >>= 1 {
-		maxDepth++
-	}
-	maxDepth *= 2
-	quickSort(result, fn, 0, n, maxDepth)
-	return result
-}
-
-// IsSorted reports whether rcv is sorted.
-func (rcv Movies) IsSorted(fn func(*Movie) string) bool {
-	n := len(rcv)
-	for i := n - 1; i > 0; i-- {
-		if less(rcv, fn, i, i-1) {
-			return false
-		}
-	}
-	return true
 }
