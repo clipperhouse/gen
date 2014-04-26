e95dee55f13433a6be590eabfcd5e50e67a97882
Matt Sherman <mwsherman@gmail.com>
2014-04-26 14:20:47 -0600

    initial implementation of typewriter architecture; lots to do

--- main.go
+++ main.go
@@ -1,78 +1,34 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"github.com/clipperhouse/gen/templates"
-	_ "github.com/clipperhouse/gen/templates/container"
-	_ "github.com/clipperhouse/gen/templates/projection"
-	_ "github.com/clipperhouse/gen/templates/standard"
+	"gen/typewriter"
+	_ "gen/typewriters/standard"
 	"os"
 )
 
-var errs = make([]error, 0)
-var standardTemplates, projectionTemplates, containerTemplates templates.TemplateSet
-
-func init() {
-	if ts, err := templates.Get("standard"); err == nil {
-		standardTemplates = ts
-	} else {
-		errs = append(errs, err)
-	}
-
-	if ts, err := templates.Get("projection"); err == nil {
-		projectionTemplates = ts
-	} else {
-		errs = append(errs, err)
-	}
-
-	if ts, err := templates.Get("container"); err == nil {
-		containerTemplates = ts
-	} else {
-		errs = append(errs, err)
-	}
-}
-
 func main() {
-	args := os.Args[1:]
-	opts, err := parseArgs(args)
-
+	pkgs, err := typewriter.GetPackages()
 	if err != nil {
 		fmt.Println(err)
-		return // command-line errors are fatal, other errors can be forced
-	}
-
-	if opts.Help {
-		fmt.Println(usage)
 		return
 	}
-
-	packages := getPackages()
-
-	if len(errs) > 0 {
-		for _, e := range errs {
-			fmt.Printf("  error: %v\n", e)
-		}
-		if opts.Force {
-			fmt.Println("  forced...")
-		} else {
-			fmt.Println("  operation canceled")
-			fmt.Println("  use the -f flag if you wish to force generation (i.e., ignore errors)")
-			return
+	for _, p := range pkgs {
+		for _, t := range p.Types {
+			writeFile(t)
 		}
 	}
-
-	writeFiles(packages, opts)
 }
 
-func addError(text string) {
-	errs = append(errs, errors.New(text))
+func writeFile(t typewriter.Type) {
+	f, err := os.Create(t.LocalName() + "_gen.go")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	typewriter.Write(f, t)
 }
 
-const usage = `Documentation is available at http://clipperhouse.github.io/gen
-
-Usage: gen [-force]
-  -force    # forces generation to continue despite errors; voids warranty; shortcut -f
-`
-
-const deprecationUrl = `http://clipperhouse.github.io/gen/#Changelog`
+// +gen
+type Silly int
