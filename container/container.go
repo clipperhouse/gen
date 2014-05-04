a158c94a8cf2685c91c669ca654838122c96c18f
Matt Sherman <mwsherman@gmail.com>
2014-05-04 13:04:15 -0400

    implement Write for containers typewriter

--- container/container.go
+++ container/container.go
@@ -15,11 +15,18 @@ func (s ContainerWriter) Name() string {
 	return "container"
 }
 
+var tagsByType = make(map[string]typewriter.Tag)
+
 func (s ContainerWriter) Validate(t typewriter.Type) (bool, error) {
-	return false, nil
+	tag, found, err := t.Tags.ByName("containers")
+	if found && err == nil {
+		tagsByType[t.String()] = tag
+	}
+	return found, err
 }
 
 func (s ContainerWriter) WriteHeader(w io.Writer, t typewriter.Type) {
+	// TODO: add license
 	return
 }
 
@@ -28,5 +35,19 @@ func (s ContainerWriter) Imports(t typewriter.Type) (result []string) {
 }
 
 func (s ContainerWriter) Write(w io.Writer, t typewriter.Type) {
+	tag := tagsByType[t.String()] // validated above
+
+	for _, c := range tag.Items {
+		tmpl, err1 := containerTemplates.Get(c)
+		if err1 != nil {
+			panic(err1)
+		}
+
+		err2 := tmpl.Execute(w, t)
+		if err2 != nil {
+			panic(err2)
+		}
+	}
+
 	return
 }
