91315d9c4b31b12044fca707710afa8801f5138d
Matt Sherman <mwsherman@gmail.com>
2014-05-11 17:59:53 -0400

    prevent existing test & gen files from running test

--- genwriter/test/setup.go
+++ genwriter/test/setup.go
@@ -4,11 +4,18 @@ package main
 
 import (
 	"github.com/clipperhouse/typewriter"
+	"os"
+	"strings"
 	_ "typewriters/genwriter" // make sure typewriters folder is at top of GOPATH/src
 )
 
 func main() {
-	a, err := typewriter.NewApp("+gen")
+	// don't let bad test or gen files get us stuck
+	filter := func(f os.FileInfo) bool {
+		return !strings.HasSuffix(f.Name(), "_test.go") && !strings.HasSuffix(f.Name(), "_gen.go")
+	}
+
+	a, err := typewriter.NewAppFiltered("+gen", filter)
 	if err != nil {
 		panic(err)
 	}
