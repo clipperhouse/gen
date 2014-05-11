8601e71c350a1ed447d4af57342867379feb205c
Matt Sherman <mwsherman@gmail.com>
2014-05-10 20:03:32 -0400

    move stuff into methods file

---
+++ genwriter/methods.go
@@ -0,0 +1,93 @@
+package genwriter
+
+import (
+	"fmt"
+	"github.com/clipperhouse/typewriter"
+	"regexp"
+	"strings"
+)
+
+// This business exists because I overload the methods tag to specify both standard and projection methods.
+// Kind of a mess, but for the end user, arguably simpler. And arguably not.
+func evaluateTags(tags typewriter.Tags) (standardMethods, projectionMethods []string, err error) {
+	var nilMethods, nilProjections bool
+
+	methods, found, methodsErr := tags.ByName("methods")
+
+	if methodsErr != nil {
+		err = methodsErr
+		return
+	}
+
+	nilMethods = !found // non-existent methods tag is different than empty
+
+	_, found, projectionsErr := tags.ByName("projections")
+
+	if projectionsErr != nil {
+		err = projectionsErr
+		return
+	}
+
+	nilProjections = !found
+
+	if nilMethods || methods.Negated {
+		// default to all
+		standardMethods = standardTemplates.GetAllKeys()
+		if !nilProjections {
+			projectionMethods = projectionTemplates.GetAllKeys()
+		}
+	}
+
+	if !nilMethods {
+		// categorize subsetted methods as standard or projection
+		std := make([]string, 0)
+		prj := make([]string, 0)
+
+		for _, m := range methods.Items {
+			isStd := standardTemplates.Contains(m)
+			if isStd {
+				std = append(std, m)
+			}
+
+			// only consider projection methods in presence of projected types
+			isPrj := !nilProjections && projectionTemplates.Contains(m)
+			if isPrj {
+				prj = append(prj, m)
+			}
+
+			if !isStd && !isPrj {
+				err = fmt.Errorf("method %s is unknown", m)
+				return
+			}
+		}
+
+		if methods.Negated {
+			standardMethods = remove(standardMethods, std...)
+			projectionMethods = remove(projectionMethods, prj...)
+		} else {
+			standardMethods = std
+			projectionMethods = prj
+		}
+	}
+
+	return
+}
+
+func includeSortSupport(standardMethods []string) bool {
+	for _, m := range standardMethods {
+		if strings.HasPrefix(m, "SortBy") {
+			return true
+		}
+	}
+	return false
+}
+
+func includeSortInterface(standardMethods []string) bool {
+	reg := regexp.MustCompile(`^Sort(Desc)?$`)
+	for _, m := range standardMethods {
+		if reg.MatchString(m) {
+			return true
+		}
+	}
+	return false
+}
