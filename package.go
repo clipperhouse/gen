06d2f417335e1789beb97dd1f2ab907b85fb0617
Matt Sherman <mwsherman@gmail.com>
2014-05-28 00:12:42 -0400

    add NewPackage for convenience

--- package.go
+++ package.go
@@ -9,6 +9,12 @@ type Package struct {
 	*types.Package
 }
 
+func NewPackage(path, name string) Package {
+	return Package{
+		types.NewPackage(path, name),
+	}
+}
+
 func (p *Package) Eval(name string) (result Type, err error) {
 	t, _, typesErr := types.Eval(name, p.Package, p.Scope())
 	if typesErr != nil {
