1324a165dd52fc324ccbed1cb587177f1207b1fe
Matt Sherman <mwsherman@gmail.com>
2014-05-28 00:28:02 -0400

    more tag parse tests

--- tag_test.go
+++ tag_test.go
@@ -10,13 +10,31 @@ func TestParseTag(t *testing.T) {
 	tag, found := parseTag(s)
 
 	if !found {
-		t.Errorf("tag foo should have been found in %s", tag)
+		t.Errorf("tag foo should have been found in %s", s)
 	}
 
 	should := []string{"bar", "Baz"}
 
 	if !sliceEqual(tag.Items, should) {
 		t.Errorf("Items should be %v, got %v", should, tag.Items)
+	}
+
+	s2 := ""
+	tag2, found2 := parseTag(s2)
+
+	if found2 {
+		t.Errorf("empty tags should not have been found in %s", tag2)
+	}
+
+	s3 := `foo:"-Baz,qaz"`
+	tag3, found3 := parseTag(s3)
+
+	if !found3 {
+		t.Errorf("tag foo should have been found in %s", s3)
+	}
+
+	if !tag3.Negated {
+		t.Errorf("tag %s should be negated", tag2)
 	}
 }
 
