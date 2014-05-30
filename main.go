cb45a2ac901e504e286bd5af3f6d339cb85920ff
Matt Sherman <mwsherman@gmail.com>
2014-05-30 01:05:27 -0400

    use tempdir; go run instead of go build + exec; no need to record if dir created

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
 )
 
 func main() {
@@ -15,53 +16,38 @@ func main() {
 		// maybe silently fail here? some people may not use this feature at all
 		// log.Println("no custom _gen.go for imports found")
 	}
-	// create ._gen dir
-	createdDir := true
-	err = os.Mkdir("._gen", 0777)
-	if err != nil {
-		createdDir = false
-		log.Println(err)
-	}
-	// panic safe dir removal?
-	defer func() {
-		// don't blow away a dir we didn't create
-		if createdDir {
-			err = os.RemoveAll("._gen")
-			if err != nil {
-				panic(err)
-			}
-		}
-	}()
+
 	// minimal compiling file if none provided
 	if len(custom) == 0 {
 		custom = []byte("package main")
 	}
-	// write custom_gen file to ._gen folder
-	err = ioutil.WriteFile("._gen/gen_custom.go", custom, 0644)
+
+	caller := path.Base(os.Args[0])
+	tempDir, err := ioutil.TempDir("", caller)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer os.RemoveAll(tempDir)
+
+	// write custom_gen file to temp folder
+	err = ioutil.WriteFile(path.Join(tempDir, "gen_custom.go"), custom, 0644)
 	if err != nil {
 		panic(err)
 	}
-	// write gen.go template to ._gen folder
-	err = ioutil.WriteFile("._gen/gen.go", []byte(gentemplate), 0644)
+
+	// write gen.go template to temp folder
+	err = ioutil.WriteFile(path.Join(tempDir, "gen.go"), []byte(gentemplate), 0644)
 	if err != nil {
 		panic(err)
 	}
+
 	var out bytes.Buffer
 	var outerr bytes.Buffer
-	// build new gen
-	cmd := exec.Command("go", "build", "-o", "._gen/gen", "._gen/gen.go", "._gen/gen_custom.go")
-	cmd.Stdout = &out
-	cmd.Stderr = &outerr
-	err = cmd.Run()
-	if err != nil {
-		log.Println(outerr.String())
-		panic(err)
-	}
-	if out.Len() > 0 {
-		log.Println(out.String())
-	}
+
 	// run new gen
-	cmd = exec.Command("._gen/gen", os.Args...)
+	cmd := exec.Command("go", "run", path.Join(tempDir, "gen.go"), path.Join(tempDir, "gen_custom.go"))
 	cmd.Stdout = &out
 	cmd.Stderr = &outerr
 	err = cmd.Run()
