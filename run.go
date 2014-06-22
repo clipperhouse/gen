5b6b3cc8ab9f6a23969336cf9e66b6a86e760430
Matt Sherman <mwsherman@gmail.com>
2014-06-22 18:09:13 -0400

    rearrange files

---
+++ run.go
@@ -0,0 +1,40 @@
+package main
+
+import "github.com/clipperhouse/gen/typewriter"
+
+func run(customFilename string) error {
+	imports := []string{
+		`"log"`,
+		`"github.com/clipperhouse/gen/typewriter"`,
+	}
+
+	return execute(runStandard, customFilename, imports, runBody)
+}
+
+func runStandard() error {
+	app, err := typewriter.NewApp("+gen")
+
+	if err != nil {
+		return err
+	}
+
+	if err := app.WriteAll(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const runBody string = `
+func main() {
+	app, err := typewriter.NewApp("+gen")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.WriteAll(); err != nil {
+		log.Fatal(err)
+	}
+}
+`
