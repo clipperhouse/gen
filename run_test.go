1ef6c4db1ee1d9b19f654ca0381a018f97041385
Matt Sherman <matt@stackoverflow.com>
2014-11-26 15:57:53 -0500

    tighten up file handling on test

--- run_test.go
+++ run_test.go
@@ -18,34 +18,31 @@ func TestRun(t *testing.T) {
 	sliceName := "dummy_slice_test.go"
 	fooName := "dummy_foo_test.go"
 
-	// remove existing files, start fresh
-	os.Remove(c.customName)
-	os.Remove(sliceName)
-	os.Remove(fooName)
-
 	// standard run
 	if err := runMain([]string{"gen"}); err != nil {
 		t.Error(err)
 	}
 
 	// gen file should exist
-	if _, err := os.Open(sliceName); err != nil {
+	if _, err := os.Stat(sliceName); err != nil {
 		t.Error(err)
 	}
 
 	// foo file should not exist, not a standard typewriter
-	if _, err := os.Open(fooName); err == nil {
+	if _, err := os.Stat(fooName); err == nil {
 		t.Errorf("%s should not have been generated", fooName)
 	}
 
 	// remove just-gen'd file
-	os.Remove(sliceName)
+	if err := os.Remove(sliceName); err != nil {
+		t.Fatal(err)
+	}
 
 	// create a custom typewriter import file
 	w, err := os.Create(c.customName)
 
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	p := pkg{
@@ -56,7 +53,11 @@ func TestRun(t *testing.T) {
 	}
 
 	if err := tmpl.Execute(w, p); err != nil {
-		t.Error(err)
+		t.Fatal(err)
+	}
+
+	if err := w.Close(); err != nil {
+		t.Fatal(err)
 	}
 
 	// custom run
@@ -64,19 +65,23 @@ func TestRun(t *testing.T) {
 		t.Error(err)
 	}
 
+	// clean up custom file, no longer needed
+	if err := os.Remove(c.customName); err != nil {
+		t.Fatal(err)
+	}
+
 	// foo file should exist
-	if _, err := os.Open(fooName); err != nil {
+	if _, err := os.Stat(fooName); err != nil {
 		t.Error(err)
 	}
 
-	// gen file should not exist, because it was not included in the custom file
-	if _, err := os.Open(sliceName); err == nil {
-		t.Errorf("%s should not have been generated", sliceName)
+	// clean up foo file
+	if err := os.Remove(fooName); err != nil {
+		t.Fatal(err)
 	}
 
-	// remove just-gen'd file
-	os.Remove(fooName)
-
-	// remove custom file
-	os.Remove(c.customName)
+	// gen file should not exist, because it was not included in the custom file
+	if _, err := os.Stat(sliceName); err == nil {
+		t.Errorf("%s should not have been generated", sliceName)
+	}
 }
