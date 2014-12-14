0839b6ac53f4cb4e8808bee92118f3958cd56b11
Matt Sherman <mwsherman@gmail.com>
2014-12-14 12:39:17 -0500

    add watch to usage; other usage changes

--- help.go
+++ help.go
@@ -30,11 +30,13 @@ type helpInfo struct {
 
 var helpTmpl = template.Must(template.New("help").Parse(`
 Usage:
-  {{.Name}}           Generate files for types marked with +gen.
-  {{.Name}} get       Download and install typewriters (standard or custom). 
-  {{.Spacer}}           Optional flags from go get: [-d] [-fix] [-t] [-u].
-  {{.Name}} list      List available typewriters (standard or custom).
+  {{.Name}}           Generate files for types marked with +{{.Name}}.
+  {{.Name}} list      List available typewriters.
   {{.Name}} add       Add a third-party typewriter to the current package.
+  {{.Name}} get       Download and install imported typewriters. 
+  {{.Spacer}}           Optional flags from go get: [-d] [-fix] [-t] [-u].
+  {{.Name}} watch     Watch the current directory for file changes, run {{.Name}}
+  {{.Spacer}}           when detected. 
   {{.Name}} help      Print usage.
 
 Further details are available at http://clipperhouse.github.io/gen
